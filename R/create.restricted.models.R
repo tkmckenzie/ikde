30c97418b7ca0034aa2fc2347087ac150656701e
tmcken <tmcken@sandia.gov>
2018-12-12 16:15:18 -0700

    Added restricted model creation

--- R/create.restricted.models.R
+++ R/create.restricted.models.R
@@ -4,7 +4,7 @@
 #' 
 #' @param ikde.model An object of class ikde.model, does not necessarily have to be built
 #' 
-#' @return Returns a list of ikde.models for each restricted model
+#' @return Returns a list of built ikde.models for each restricted model
 #' 
 #' @details 
 #' 
@@ -29,10 +29,38 @@
 #' @export
 
 create.restricted.models <-
-  function(ikde.model){
+  function(ikde.model, eval.point){
     if (class(ikde.model) != "ikde.model") stop("ikde.model must be of class \"ikde.model\".")
     
+    current.ikde.model <- ikde.model
+    model.list <- list(ikde.model)
+    num.parameters <- length(ikde.model$parameters)
+    for (parameter in names(ikde.model$parameters)[-num.parameters]){
+      parameter.type <- ikde.model$parameters[[parameter]]
+      parameter.type <- gsub(" ", "", parameter.type)
+      if (grepl("vector", parameter.type)){
+        #TODO: Get parameter restrictions
+        vector.length.pos <- gregexpr("(?<=vector\\[)[0-9A-Za-z\\.,\\*/\\+-\\^]+(?=\\])", parameter.type, perl = TRUE)[[1]]
+        vector.length <- substr(parameter.type, as.numeric(vector.length.pos), as.numeric(vector.length.pos) + attr(vector.length.pos, "match.length") - 1)
+        for (data.var in names(ikde.model$data)){
+          vector.length <- gsub(data.var, paste0("ikde.model$data$", data.var, "[[2]]"), vector.length)
+        }
+        vector.length <- evaluate.expression(vector.length)
+        
+        parameter.restr <- paste0(parameter, "_restr")
+        parameter.unrestr <- paste0(parameter, "_unrestr")
+        
+        for (vector.index in 1:vector.length){
+          next.ikde.model <- current.ikde.model
+          next.ikde.model$parameters[[parameter]] <- NULL #Remove from parameters list
+          next.ikde.model$data[[parameter.restr]] <- list(paste0("vector[", vector.index, "]"),
+                                                                      eval.point[[parameter]][1:vector.index]) #Add restricted values to data
+          next.ikde.model$parameters[[parameter.unrestr]] <- paste0("vector[", vector.length - vector.index, "]") #Add unrestricted values to parameters
+          next.ikde.model$transformed.parameters <- append(list())
+          
+        }
+      }
+    }
     
-    
-    return(ikde.model)
+    return(model.list)
   }
